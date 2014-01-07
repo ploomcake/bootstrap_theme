61e7d56391910f31f07bec098521449f9e6a6014
root <root@gd-f-2>
2014-01-07 12:40:02 +0100

    a base theme based on twitter bootstrap 3.0

---
+++ ploomcake/bootstrap_theme/__init__.py
@@ -0,0 +1,5 @@
+# -*- extra stuff goes here -*-
+import transforms
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
---
+++ ploomcake/bootstrap_theme/browser/interfaces.py
@@ -0,0 +1,6 @@
+from ploomcake.theme.browser.interfaces import IPloomCakeThemeSpecific
+from zope.interface import Interface
+from zope.viewlet.interfaces import IViewletManager
+
+class IBootstrapThemeSpecific(IPloomCakeThemeSpecific):
+    """ """
